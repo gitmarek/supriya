6bf02f9fac034a63b23438e52d75105538052157
Marek Miller <marek.miller@protonmail.com>
2022-11-26 16:11:42 +0100

    Annoate test/proptest/setup.py

--- tests/proptest/setup.py
+++ tests/proptest/setup.py
@@ -1,14 +1,19 @@
-from typing import Optional
+from typing import Any, Callable, Optional, ParamSpec
 
 import hypothesis
 import hypothesis.strategies as st
 
+F = Callable[..., Any]
+P = ParamSpec("P")
+
 hp_global_settings = hypothesis.settings()
 
 
-def get_control_test_groups(min_size: int = 1, max_size: Optional[int] = None):
-    def _wrapper(func):
-        def _st_func(*args, **kwargs):
+def get_control_test_groups(
+    min_size: int = 1, max_size: Optional[int] = None
+) -> Callable[[F], F]:
+    def _wrapper(func: F) -> F:
+        def _st_func(*args: P.args, **kwargs: P.kwargs) -> st.SearchStrategy[Any]:
             strategy = func(*args, **kwargs)
             return st.tuples(
                 st.lists(strategy, min_size=min_size, max_size=max_size),
